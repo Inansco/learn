bb04a37cdae61e68f84390f9ca92e37c9c13d7dc
Inansco <mondayprince20@gmail.com>
2026-04-18 06:40:00 +0100

    .

---
+++ go-reloaded/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	if len(os.Args) != 3 {
+		fmt.Println("use: go run . input.txt output.txt")
+		return
+	}
+
+	input := os.Args[1]
+	output := os.Args[2]
+
+	file, err := os.ReadFile(input)
+	if err != nil {
+		fmt.Println("Error")
+		return
+	}
+
+	data := (string(file))
+
+	err = os.WriteFile(output, []byte(data), 0644)
+	if err != nil {
+		fmt.Println("Error")
+		return 
+	}
+	fmt.Println("It is done!!!!!!!")
+}
