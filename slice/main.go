d20e33d39122d44c9f7b2d7f615fc675d85b5f2e
mefada <mondayprince20@gmail.com>
2026-04-28 06:52:14 +0100

    .

--- slice/main.go
+++ slice/main.go
@@ -1,1 +1,16 @@
-package slice
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	nums := []int{10, 20, 30}
+
+	for _, v := range nums {
+		fmt.Println(v)
+	}
+	for i := 0; i < len(nums); i++ {
+    fmt.Println(nums[i])
+}
+}
