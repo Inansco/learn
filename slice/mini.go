b182a3fea4afced20231a9ecf3eeb7a0e7c4ada1
mefada <mondayprince20@gmail.com>
2026-04-28 07:12:18 +0100

    .

---
+++ slice/mini.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	slice := []string{"Emma", "John", "Daniel"}
+	for i := 0; i < len(slice); i++ {
+		fmt.Println(slice[i])
+	}
+	
+	text := "Hello World From Go"
+
+	
+
+}
