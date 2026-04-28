346539f46bc7c3a6426bf513af5cf59428a201a5
mefada <mondayprince20@gmail.com>
2026-04-28 06:59:50 +0100

    .

--- slice/main.go
+++ slice/main.go
@@ -2,15 +2,27 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 func main() {
 	nums := []int{10, 20, 30}
 
-	for _, v := range nums {
-		fmt.Println(v)
+	for i, v := range nums {
+		fmt.Println(i, v)
 	}
 	for i := 0; i < len(nums); i++ {
-    fmt.Println(nums[i])
-}
+		fmt.Println(nums[i])
+	}
+
+	words := []string{"Hello", "World"}
+	fmt.Println(words[0]) // Hello
+
+	//Reading a file and turning it into lines:\
+	content := "line1\nline2\nline3"
+
+	lines := strings.Split(content, "\n")
+
+	fmt.Println(lines)
+	// ["line1", "line2", "line3"]
 }
