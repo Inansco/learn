ebaabf58e0a3c8506f19554b425773b0dffbe7a5
mefada <mondayprince20@gmail.com>
2026-05-23 16:48:01 +0100

    exercise1

---
+++ study/exercise1.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name = "Monday"
+	age := 30
+	food := "Beans"
+
+	fmt.Printf("My name is %s \n", name)
+	fmt.Printf("I am %d years old \n", age)
+	fmt.Printf("My favorite food is %s \n", food)
+}
