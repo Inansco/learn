288677e111635d2d61fedb3da6aaa7f52d7f5fa3
Inansco <mondayprince20@gmail.com>
2026-09-07 16:16:34 +0100

    .

---
+++ python/main.py
@@ -0,0 +1,15 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+department = input("Enter your department: ")
+score = int(input("Enter your score: "))
+bonus = 5
+final_score = score + bonus
+
+print("===== STUDENT INFORMATION =======\n")
+
+print(f"Name: {name}")
+print(f"Age: {age}")
+print(f"Department: {department}")
+print(f"Score: {score}")
+print(f"Bonus: {bonus}")
+print(f"Final Score: {final_score}")
