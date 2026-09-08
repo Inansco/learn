3dc8b50dbefc5aafa3d29e73a67c662484ce3f01
Inansco <mondayprince20@gmail.com>
2026-09-08 18:19:06 +0100

    .

---
+++ python/4.py
@@ -0,0 +1,10 @@
+word = input("Enter a word: ")
+
+print("===== STRING ANALYZER =====\n")
+
+print(f"Word: {word}")
+print(f"First character: {word[0]}")
+print(f"Last character: {word[-1]}")
+print(f"First 3 characters: {word[:3]}")
+print(f"Last 3 characters: {word[-3:]}")
+print(f"Reversed: {word[::-1]}")
