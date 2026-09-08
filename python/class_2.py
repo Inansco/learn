daa513a231f5f9a01534f6058aea568198ea900d
Inansco <mondayprince20@gmail.com>
2026-09-08 17:41:38 +0100

    .

---
+++ python/class_2.py
@@ -0,0 +1,17 @@
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+print("===== CALCULATOR =====\n")
+print(f"Addition: {num1 + num2}")
+print(f"Subtraction: {num1 - num2}")
+print(f"Multiplication: {num1 * num2}")
+print(f"Division: {num1 / num2}")
+print(f"Floor Division: {num1 // num2}")
+print(f"Remainder: {num1 % num2}")
+print(f"Power: {num1 ** num2}")
+
+age = int(input("Enter your age: "))
+
+print(f"Your age: {age}")
+print(f"Your age in 5 years: {age + 5}")
+print(f"Your age in 10 years: {age + 10}")
